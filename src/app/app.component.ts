cd650feb5f57704535abb0caf615784c7e020110
JaimeNM0 <navarro.m.jai@gmail.com>
2024-01-15 22:22:05 +0100

    Se han modificado formularios.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { OptionsButtonComponent } from './options-button/options-button.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ElementFormComponent } from './element-form/element-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, OptionsButtonComponent],
+  imports: [CommonModule, RouterOutlet, OptionsButtonComponent, LoginFormComponent, ElementFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/element-form/element-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-element-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './element-form.component.html',
+  styleUrl: './element-form.component.css'
+})
+export class ElementFormComponent {
+
+}
